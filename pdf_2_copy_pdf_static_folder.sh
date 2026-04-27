fce1ff2cab252d91aa07f9661e1a5fdf65e412c5
Mike Stanley <stanley50@marshall.edu>
2026-04-27 17:57:11 -0400

    Separate the pdf theme folder into its own folder.

--- pdf_2_copy_pdf_static_folder.sh
+++ pdf_2_copy_pdf_static_folder.sh
@@ -1,1 +1,2 @@
 purple_mercury copy_pdf_static_folder --pdf_static_folder "../Operations_Manual/pdf_static/"  --pdf_build_folder "../Operations_Manual/pdf/" --log_level 30 --log_name "logs/pdf_2.log"
+purple_mercury copy_pdf_static_folder --pdf_static_folder "../Operations_Manual/pdf_theme/"  --pdf_build_folder "../Operations_Manual/pdf/" --log_level 30 --log_name "logs/pdf_2.log"
