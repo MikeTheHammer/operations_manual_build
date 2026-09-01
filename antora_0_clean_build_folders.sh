727bfa76c7974dcf0470e707814e8f21abe3898d
Mike Stanley <stanley50@marshall.edu>
2026-09-01 11:51:22 -0400

    Include forgotten foled when cleaning for Antora.

--- antora_0_clean_build_folders.sh
+++ antora_0_clean_build_folders.sh
@@ -1,4 +1,5 @@
 purple_mercury clean_build_folder --build_folder "../operations_manual_source/antora_intermediate/" --log_level 30 --log_name "logs/antora_0.log"
 purple_mercury clean_build_folder --build_folder "../operations_manual_source/antora/modules/ROOT/pages/" --log_level 30 --log_name "logs/antora_0.log"
 purple_mercury clean_build_folder --build_folder "../operations_manual_source/renders/web/" --log_level 30 --log_name "logs/antora_0.log"
+purple_mercury clean_build_folder --build_folder "/mnt/T-Operations_Storage/Operations/ops_manual/" --log_level 30 --log_name "logs/antora_0.log"
 purple_mercury clean_build_folder --build_folder "../operations_manual/web" --log_level 30 --log_name "logs/antora_0.log"
